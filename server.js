9d46fe701a38b15d6c5376801f017f613e47d474
Bailey <bailey.siber@tufts.edu>
2018-09-28 18:56:47 -0400

    modifying server.js

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-// const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI);
-//
-// const Message = mongoose.model('Message', {
-//   name: String,
-//   email: String,
-//   subject: String,
-//   message: String
-// })
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(bodyParser.json())
 
-// app.get('/api/test', function(req, res) {
-//   res.send('success');
-// });
-//
-// app.post('/api/contact', function (req, res) {
-//   console.log(req.body)
-//   new Message(req.body)
-//     .save()
-//     .then((result) => res.json(result))
-//     .catch((err) => res.status(500).end(err.message))
-// });
+app.use(express.static(__dirname + '/scss'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname +'/scss'));
+});
+
 
 
 app.listen(process.env.PORT || 3001);
