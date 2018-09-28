3700160cd7a834cba4b167ab1c25bd0475a651e8
Bailey <bailey.siber@tufts.edu>
2018-09-28 18:29:50 -0400

    ugh

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI);
+//
+// const Message = mongoose.model('Message', {
+//   name: String,
+//   email: String,
+//   subject: String,
+//   message: String
+// })
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json())
+
+// app.get('/api/test', function(req, res) {
+//   res.send('success');
+// });
+//
+// app.post('/api/contact', function (req, res) {
+//   console.log(req.body)
+//   new Message(req.body)
+//     .save()
+//     .then((result) => res.json(result))
+//     .catch((err) => res.status(500).end(err.message))
+// });
+
+
+app.listen(process.env.PORT || 3001);
