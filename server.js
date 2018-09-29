4495eaf96529149b464da24c679416b001a9865d
Bailey <bailey.siber@tufts.edu>
2018-09-29 15:21:31 -0400

    styling changes

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(__dirname + '/scss'));
+app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname +'/scss'));
+res.sendFile(__dirname + "/index.html");
 });
-
-
 
 app.listen(process.env.PORT || 3001);
