4afb697b1c0d040a5df19ff1201669cbb88d07ba
Bailey <bailey.siber@tufts.edu>
2018-11-16 21:23:03 -0800

    pinging

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var http = require("http");
+setInterval(function() {
+    http.get("https://siberconsultingsite.herokuapp.com/");
+}, 1500000);
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
